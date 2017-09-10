5fa66bc1d78ac8ad0b57c2900c9e60f40991f3a8
Leon Mak <leonmakansheng@gmail.com>
2017-09-10 11:53:07 -0700

    Add form publishing

--- ReQuest_ios/ReQuest_ios/Managers/Satori.swift
+++ ReQuest_ios/ReQuest_ios/Managers/Satori.swift
@@ -13,17 +13,18 @@ public class Satori {
     // Singleton for Satori
     static let instance = Satori()
     public var conn: SatoriRtmConnection? = nil
+    var handler: PduHandler?
+    let appKey = "E8eA0fEc0B3a8af1ca39b0aBADc0F18d"
+    let url : String = "wss://kpbbao2l.api.satori.com"
+    let channelName = "test-feed"
 
     func initialize() {
         setupSatori()
     }
     
     func setupSatori() {
-        let appKey = "E8eA0fEc0B3a8af1ca39b0aBADc0F18d"
-        let url : String = "wss://kpbbao2l.api.satori.com"
-        let channelName = "test-feed"
         //: __Define the PduHandler. This will be called by Satori rtm when there is activity for the subscribe success/error response and subscription data responses.__
-        let handler : PduHandler = {(SatoriPdu) -> Void in
+        handler = {(SatoriPdu) -> Void in
             let action : rtm_action_t = SatoriPdu.action;
             switch action {
             case RTM_ACTION_SUBSCRIPTION_DATA:
@@ -39,13 +40,47 @@ public class Satori {
         }
         //: __Connect to Satori using SatoriRtmConnection. And subscribe to channel to show title and published on date in real time__
         DispatchQueue.global(qos: .background).async {
-            self.conn = SatoriRtmConnection(url: url, andAppkey: appKey);
-            self.conn?.connect(pduHandler: handler)
+            self.conn = SatoriRtmConnection(url: self.url, andAppkey: self.appKey);
+            self.conn?.connect(pduHandler: self.handler!)
             var requestId:UInt32 = 123;
-            self.conn?.subscribe(channelName, andRequestId: &requestId);
+            self.conn?.subscribe(self.channelName, andRequestId: &requestId);
             while ((self.conn?.poll().rawValue)! >= 0) {
                 sleep(1);
             }
         }
+    }
+    
+    func jsonToString(json: AnyObject) -> String {
+        do {
+            let data =  try JSONSerialization.data(withJSONObject: json, options: JSONSerialization.WritingOptions.prettyPrinted)
+            return String(data: data, encoding: String.Encoding.utf8)!
+        } catch let myJSONError {
+            print(myJSONError)
+        }
+        
+        return ""
+    }
+
+
+    func publish(quest: Quest) {
+        DispatchQueue.global(qos: .background).async {
+            self.conn?.connect(pduHandler: self.handler!)
+            var id:UInt32 = 123
+            let bytesPointer = UnsafeMutablePointer<UInt32>(&id)
+
+            let jsonObject = [
+                [
+                "usedId": quest.creator.id,
+                "lat": quest.lat,
+                "lng": quest.lng,
+                "direction": "offer",
+                "type": quest.category.rawValue
+                ]
+            ]
+
+            self.conn?.publishString(self.jsonToString(json: jsonObject as AnyObject),
+                                     toChannel: self.channelName, andRequestId: bytesPointer)
+        }
+
     }
 }
--- ReQuest_ios/ReQuest_ios/Model/QuestCategory.swift
+++ ReQuest_ios/ReQuest_ios/Model/QuestCategory.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum QuestCategory {
+enum QuestCategory: String {
     case Food
     case Labour
     case Tourist
@@ -31,5 +31,19 @@ enum QuestCategory {
         case .Labour, .Tourist, .Uncategorized:
             return "user_pin"
         }
+    }
+    
+    static func labelToCategory(_ label: String) -> QuestCategory {
+        switch label {
+        case "Food": return .Food
+        case "Labour": return .Labour
+        case "Tourist": return .Tourist
+        case "Transportation": return .Transportation
+        default: return .Uncategorized
+        }
+    }
+    
+    static func labels() -> [String] {
+        return ["Food", "Labour", "Tourist", "Transportation"]
     }
 }
